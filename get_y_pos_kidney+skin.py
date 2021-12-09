b79936363845817320efbf80a07b54ed19a5363b
andreasbueckle <abueckle@indiana.edu>
2021-12-09 10:54:38 -0500

    Update get_y_pos_kidney+skin.py

--- get_y_pos_kidney+skin.py
+++ get_y_pos_kidney+skin.py
@@ -5,7 +5,7 @@ import urllib.request
 def main():
     # To get TOKEN for viewing unpublished data, go to EUI, log in, then view source, then copy token from browser
     # TOKEN = sys.argv[1] if len(sys.argv) > 1 else None
-    TOKEN = 'AgvladOG76Kjw5mY3JEGXlbMeMm8yYNayJP4ledKYqoBV5Ob6NuzCMn0JG0J0WoW9pd90en6ag138zs9oeQYJtw57l'
+    TOKEN = ''
     HBM_LINK = 'https://hubmap-link-api.herokuapp.com/hubmap-datasets?format=jsonld'
     if TOKEN:
         HBM_LINK += '&token=' + TOKEN
@@ -28,7 +28,5 @@ def main():
 
         for item in [skin_datasets_male, skin_datasets_female]:
             print(item)
-
-
 
 main()
