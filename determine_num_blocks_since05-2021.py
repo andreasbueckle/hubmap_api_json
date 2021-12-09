539e91b2fdc7324e55a7db921b2b5d9b6ef8f0cd
andreasbueckle <abueckle@indiana.edu>
2021-12-09 16:58:53 -0500

    Create determine_num_blocks_since05-2021.py

---
+++ determine_num_blocks_since05-2021.py
@@ -0,0 +1,39 @@
+from datetime import date
+from typing import Counter
+import urllib.request
+import json
+from dateutil import parser
+import sys
+
+
+def main():
+
+    # To get TOKEN for viewing unpublished data, go to EUI, log in, then view source, then copy token from browser
+    # TOKEN = sys.argv[1] if len(sys.argv) > 1 else None
+    TOKEN = ''
+    HBM_LINK = 'https://hubmap-link-api.herokuapp.com/hubmap-datasets?format=jsonld'
+    if TOKEN:
+        HBM_LINK += '&token=' + TOKEN
+
+    # first, let's get the most recent data
+    with urllib.request.urlopen(HBM_LINK) as url:
+        data = json.loads(url.read().decode())
+
+        counter = 0
+        for item in data['@graph']:
+
+            for sample in item['samples']:
+                # Parse string as datetime object to detemine if in contest range
+                as_date_time = parser.parse(
+                    sample['rui_location']['creation_date'])
+                if as_date_time > parser.parse('2021-05-01 00:00:00'):
+                    counter = counter + 1
+
+        print(f'''
+Number of tissue blocks registered since May 1, 2021: {counter}
+''')
+
+# Number of tissue blocks registered since May 1, 2021: 271
+
+
+main()
